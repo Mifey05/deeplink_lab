2ecafe84f4e1654ada9a18d9b487fa880b28e360
Michael Vergo <mifey05@gmail.com>
2025-10-08 18:42:57 +0800

    checkpoint3

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,13 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+        afterEvaluate {
+        if (project.hasProperty("android")) {
+            (project.extensions.getByName("android") as com.android.build.gradle.BaseExtension).apply {
+                buildToolsVersion = "36.1.0"
+            }
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
